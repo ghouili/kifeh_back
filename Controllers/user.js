617f8f4e7c806dc33178ccb44445a794d8f00fae
ghouili <walid.ghouili98@gmail.com>
2023-04-19 09:08:49 +0100

    19/04

--- Controllers/user.js
+++ Controllers/user.js
@@ -2,26 +2,22 @@ const user = require('../Models/user');
 
 const bcrypt = require('bcryptjs');
 
-const Example = (req, res) => {
-    res.send('controller is working');
-}
-
 const Register = async (req, res) => {
     const { name, email, cin, password } = req.body;
 
     let userExist;
     try {
-        userExist = await user.findOne({ email:email});
+        userExist = await user.findOne({ email: email });
     } catch (error) {
-        return res.status(500).json({success: false, message: 'something went wrong with the server', data: error});
+        return res.status(500).json({ success: false, message: 'something went wrong with the server', data: error });
     }
-    
+
     if (userExist) {
-        return res.status(200).json({success: false, message: 'User Already Exisit!!', data: userExist});
+        return res.status(200).json({ success: false, message: 'User Already Exisit!!', data: userExist });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
-    
+
     const newUser = new user({
         name,
         email,
@@ -30,22 +26,110 @@ const Register = async (req, res) => {
     });
 
     try {
-        newUser.save();
+        await newUser.save();
     } catch (error) {
-        return res.status(500).json({success: false, message: 'something went wrong with the server while savin new user', data: error});
+        return res.status(500).json({ success: false, message: 'something went wrong with the server while savin new user', data: error });
     }
-    
-    return res.status(201).json({success: true, message: 'User Added succesfully!!!', data: newUser});
 
-    
+    return res.status(201).json({ success: true, message: 'User Added succesfully!!!', data: newUser });
+
+
 }
 
 const Login = async (req, res) => {
     // data recieve
+    const { email, password } = req.body;
+
     //check is email exist
+    let userExist;
+    try {
+        userExist = await user.findOne({ email: email });
+    } catch (error) {
+        return res.status(500).json({ success: false, message: 'something went wrong with the server', data: error });
+    }
+
+    if (!userExist) {
+        return res.status(200).json({ success: false, message: 'User doesn"t Exisit!!' });
+    }
+
     //compare password
+    const compare = await bcrypt.compare(password, userExist.password);
+
     //access or refuse
+    if (!compare) {
+        return res.status(200).json({ success: false, message: 'Check your Password!!' });
+    }
+
+    return res.status(200).json({ success: true, message: `Welcome Mr/Mrs ${userExist.name}`, data: userExist });
+
 }
 
-exports.Example = Example
+const GetAll = async (req, res) => {
+
+    let userExist;
+    try {
+        userExist = await user.find();
+    } catch (error) {
+        return res.status(500).json({ success: false, message: 'something went wrong with the server', data: error });
+    }
+
+    return res.status(200).json({ success: true, message: `success`, data: userExist });
+}
+
+const FindById = async (req, res) => {
+
+    const { id } = req.params;
+
+    let userExist;
+    try {
+        userExist = await user.findById(id);
+    } catch (error) {
+        return res.status(500).json({ success: false, message: 'something went wrong with the server', data: error });
+    }
+
+    if (!userExist) {
+        return res.status(200).json({ success: false, message: 'User doesn"t Exisit!!' });
+    }
+
+    return res.status(200).json({ success: true, message: `success`, data: userExist });
+}
+
+const Delete = async (req, res) => {
+    const { id } = req.params;
+
+    let userExist;
+    try {
+        userExist = await user.findById(id);
+    } catch (error) {
+        return res.status(500).json({ success: false, message: 'something went wrong with the server', data: error });
+    }
+
+    if (!userExist) {
+        return res.status(200).json({ success: false, message: 'User doesn"t Exisit!!' });
+    }
+
+    try {
+        await userExist.deleteOne();
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ success: false, message: 'something went wrong with the server while deleting existing user', data: error });
+    }
+
+    return res.status(200).json({ success: true, message: 'User deleted succesfully!!!' });
+}
+
+const Update = async (req, res) => {
+    //recieve data
+    //findByID
+    //update data
+    //save
+
+}
+
+
 exports.Register = Register
+exports.Login = Login
+exports.Update = Update
+exports.GetAll = GetAll
+exports.FindById = FindById
+exports.Delete = Delete
--- Routes/user.js
+++ Routes/user.js
@@ -4,8 +4,14 @@ const UserController = require('../Controllers/user');
 
 const route = express.Router();
 
-route.get('/', UserController.Example);
+route.get('/', UserController.GetAll);
+
+route.get('/:id', UserController.FindById);
+
+route.delete('/:id', UserController.Delete);
 
 route.post('/register', UserController.Register);
+
+route.post('/login', UserController.Login);
 
 module.exports = route
